67d43bc58c97205ae344b82cacb179e3659d6a54
coreydavis <corey@commercekitchen.com>
2016-07-20 13:10:15 -0600

    [ FEAT ] Names list can be sorted with buttons

--- index.js
+++ index.js
@@ -1,34 +1,22 @@
-// var oldHello = function(name) {
-//   console.log('hello, ' + name + '!');
-// };
+var names = [ "Andy Mention",
+              "Emily Davis",
+              "Eric Fransen",
+              "Jessica Goulding",
+              "Jonmichael Chambers",
+              "Marc Garreau",
+              "Tan Doan",
+              "Alan Smith",
+              "Allison Larson",
+              "Andrew Watkins",
+              "Chad Brading",
+              "Corey Davis",
+              "Gustavo Villagrana",
+              "Hilary Denton",
+              "Horacio Chavez",
+              "Tim Proctor",
+              "Will Faurot" ]
 
-
-// const hello = (name) => {
-//   console.log(`hello, ${name}!`);
-// };
-
-// oldHello('world');
-// hello('es6');
-
-function printNames(){
-  var names = [ "Andy Mention",
-                "Emily Davis",
-                "Eric Fransen",
-                "Jessica Goulding",
-                "Jonmichael Chambers",
-                "Marc Garreau",
-                "Tan Doan",
-                "Alan Smith",
-                "Allison Larson",
-                "Andrew Watkins",
-                "Chad Brading",
-                "Corey Davis",
-                "Gustavo Villagrana",
-                "Hilary Denton",
-                "Horacio Chavez",
-                "Tim Proctor",
-                "Will Faurot" ]
-
+function printNames(names){
   var arrayLength = names.length;
 
   for (var i = 0; i < arrayLength; i++) {
@@ -39,3 +27,43 @@ function printNames(){
     $('.contact-list').append(div);
   }
 };
+
+// okay this works, but I DON'T understand it . . . 
+function compare(a, b) {
+  var splitA = a.split(" ");
+  var splitB = b.split(" ");
+  var lastA = splitA[splitA.length - 1];
+  var lastB = splitB[splitB.length - 1];
+
+  if (lastA < lastB) return -1;
+  if (lastA > lastB) return 1;
+  return 0;
+};
+
+
+$(document).ready(function(){
+  printNames(names);
+
+  $('#sort-first').on('click', function() {
+    var sorted = names.slice().sort();
+    $('.contact-list').empty();
+    printNames(sorted);
+  });
+
+  $('#sort-reverse').on('click', function() {
+    var reverseSorted = names.slice().sort().reverse();
+    $('.contact-list').empty();
+    printNames(reverseSorted);
+  });
+
+  $('#sort-last').on('click', function() {
+    var lastSorted = names.slice().sort(compare);
+    $('.contact-list').empty();
+    printNames(lastSorted);
+  });
+
+  $('#remove-sort').on('click', function() {
+    $('.contact-list').empty();
+    printNames(names);
+  });
+});
